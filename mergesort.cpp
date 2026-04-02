f9a371f10c8b5db94315c171201c2659bdb9f19a
fazila7002 <fazilatunnisa705@gmail.com>
2026-04-02 11:11:44 +0700

    loop Perbandingan

--- mergesort.cpp
+++ mergesort.cpp
@@ -49,7 +49,36 @@ int mid = (low + high) / 2;
     int j = mid + 1; 
 
     int k = low;  
+
+    while (i <= mid && j <= high) 
+
+    {
+
+        if (arr[i] <= arr[j]) 
+
+        {
+
+            B[k] = arr[i];
+
+            i++;
+
+        }
+
+        else
+
+        {
+
+            B[k] = arr[j];
+
+            j++;
+
+        }
+
+        k++; // step 4.d.ii
+
+    }
 }
+
 
 
 
