a0754a2d3cad22f6d1d4c792b72d76818f188507
fazila7002 <fazilatunnisa705@gmail.com>
2026-04-02 11:09:45 +0700

    Divide & Conquer

--- mergesort.cpp
+++ mergesort.cpp
@@ -31,6 +31,19 @@ void input()
     }
 }
 
+void mergeSort(int low, int high)
+{
+    if (low >= high) 
+    {
+        return; 
+    }
+
+int mid = (low + high) / 2; 
+
+    
+    mergeSort(low, mid);     
+    mergeSort(mid + 1, high);
+}
 
 
 
